a31f7cc878b9ccab84928f3067cfbd395858f7a5
sj1801sj <sj1801sj@naver.com>
2016-03-25 23:34:59 +0900

    Added files via upload

---
+++ F_or_C.py
@@ -0,0 +1,28 @@
+﻿import turtle
+
+wn=turtle.Screen()
+
+t1=turtle.Turtle()
+
+sel=raw_input("F or C:")
+
+print sel
+
+temp=raw_input("please enter temperature:")
+
+temp=float(temp)
+
+
+if(sel=='F'):
+
+    print ((temp-32)/1.8,"C")
+
+elif(sel=='C'):
+
+    print ((temp*1.8)+32,"F")
+
+else:
+
+    print "Entered again please"
+
+wn.exitonclick()
---
+++ T_or_S.py
@@ -0,0 +1,28 @@
+﻿import turtle
+
+wn=turtle.Screen()
+
+t1=turtle.Turtle()
+
+def draw(size,angle,many):
+    t1.home()
+    t1.clear()
+    for s in range(many):
+        t1.forward(size)
+        t1.left(angle)
+
+sel=raw_input("T or S:")
+
+if(sel=='T'):
+
+    print "You entered T"
+
+    draw(100, 120, 3)
+
+else:
+
+    print "You entered S"
+
+    draw(100, 90, 4)
+
+wn.exitonclick()

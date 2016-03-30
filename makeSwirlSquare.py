20e1113f1c3745d6f997dc3c4c36b58c4ccdccf3
sj1801sj <sj1801sj@naver.com>
2016-03-30 14:18:32 +0900

    Added files via upload

---
+++ makeSwirlSquare.py
@@ -0,0 +1,16 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+def makeSwirlSquare(size,bigger,sides,angle):
+    for i in range(0,sides):
+        if i%2:
+            size=size+bigger
+        t1.fd(size)
+        t1.left(angle)
+        
+        
+        
+makeSwirlSquare(10,10,10,90)
+
+wn.exitonclick()

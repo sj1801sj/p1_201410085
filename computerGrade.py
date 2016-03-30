109328f41825f0128e1e3a0fe2bbdd44f6543c84
ParkSoJeong <sj1801sj@naver.com>
2016-03-30 15:51:54 +0900

    Added files via upload

---
+++ computerGrade.py
@@ -0,0 +1,20 @@
+﻿def computerGrade(marks):
+   
+    if(90<=marks and marks<=100):
+        grade= 'A'
+    elif(80<=marks and marks<90):
+        grade='B'
+    elif(70<=marks and marks<80):
+        grade='C'
+    elif(60<=marks and marks<70):
+        grade='D'
+    else:
+        grade='F'
+    return grade
+
+marks=raw_input("enter marks")
+marks=float(marks)
+mygrade=computerGrade(marks)
+print mygrade
+
+wn.exitonclick()

0c36f93401c95808be485e713614704c6bffa0af
ParkSoJeong <sj1801sj@naver.com>
2016-03-30 19:22:58 +0900

    Added files via upload
    4시전에 올렸던 과제에서 수정할 부분이 있어 수정하고 다시 올립니다.

--- computerGrade.py
+++ computerGrade.py
@@ -1,23 +1,18 @@
-import turtle
-wn=turtle.Screen()
-
-﻿def computerGrade(marks):
-   
-    if(90<=marks and marks<=100):
-        grade= 'A'
-    elif(80<=marks and marks<90):
-        grade='B'
-    elif(70<=marks and marks<80):
-        grade='C'
-    elif(60<=marks and marks<70):
-        grade='D'
-    else:
-        grade='F'
-    return grade
-
-marks=raw_input("enter marks")
-marks=float(marks)
-mygrade=computerGrade(marks)
-print mygrade
-
-wn.exitonclick()
+﻿def computerGrade(marks): 
+     
+    if(90<=marks and marks<=100): 
+         grade= 'A' 
+    elif(80<=marks and marks<90): 
+        grade='B' 
+    elif(70<=marks and marks<80): 
+         grade='C' 
+    elif(60<=marks and marks<70): 
+         grade='D' 
+    else: 
+         grade='F' 
+    return grade 
+marks=raw_input("enter marks") 
+marks=float(marks) 
+mygrade=computerGrade(marks) 
+print mygrade 
+marks=raw_input("If you want to Quit, press any key") 

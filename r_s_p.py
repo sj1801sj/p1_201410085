0fa9ffdb259e694ad10fe296e648c1cfa831658d
ParkSoJeong <sj1801sj@naver.com>
2016-04-01 03:49:39 +0900

    Added files via upload

---
+++ r_s_p.py
@@ -0,0 +1,37 @@
+﻿p1=raw_input("input p1")
+p2=raw_input("input p2")
+
+if p1=="r" :
+    if p2=="r":
+        print "draw"
+    elif p2=="s":
+        print "p1 win"
+    elif p2=="p":
+        print "p2 win"
+    else:
+        print "p2 error"
+        
+elif p1=="s":
+    if p2=="r":
+        print "p2 win"
+    elif p2=="s":
+        print "draw"
+    elif p2=="p":
+        print "p1 win"
+    else:
+        print "p2 error"
+        
+elif p1=="p":
+    if p2=="r":
+        print "p1 win"
+    elif p2=="s":
+        print "p2 win"
+    elif p2=="p":
+        print "draw"
+    else:
+        print "p2 error"
+        
+else:
+    print "p1 error"
+    
+raw_input("if you want to exit, press enter")

0d57fee5c3f067ff91fa0d67e4cbd7ce7e73a7c5
ParkSoJeong <sj1801sj@naver.com>
2016-04-04 19:43:39 +0900

    bmi

---
+++ w5Main3.py
@@ -0,0 +1,25 @@
+﻿height=raw_input("What is your height?")
+weight=raw_input("What is your weight?")
+
+height=float(height)
+weight=float(weight)
+
+heightM=height/100
+
+bmi=weight/(heightM*heightM)
+
+print bmi
+
+def computebmi(height, weight):
+    if bmi<18.5:
+        print "Under weight"
+    elif bmi>=18.5 and bmi<23.0:
+        print "normal weight"
+    elif bmi>=23.0 and bmi<25.0:
+        print "overweight"
+    else:
+        print "obese"
+        
+computebmi(height, weight)
+        
+raw_input()

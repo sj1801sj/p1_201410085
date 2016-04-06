8878df53281d98769ace48ffbf65e2536133f570
ParkSoJeong <sj1801sj@naver.com>
2016-04-06 15:42:35 +0900

    leapyear

---
+++ w6Main3.py
@@ -0,0 +1,12 @@
+﻿
+
+def leapyear():
+    sel=int(raw_input("press the year"))
+    if (sel%4 == 0) and (sel%100 !=0 or sel%400 == 0):
+        print "this year is leap year"
+    else:
+        print "this year is not leap year"
+
+leapyear()
+
+raw_input()

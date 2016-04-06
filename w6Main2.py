7820d7b6f27472280e87dd67c82e213f8627964a
ParkSoJeong <sj1801sj@naver.com>
2016-04-06 15:16:44 +0900

    3과 5의 배수의 합

---
+++ w6Main2.py
@@ -0,0 +1,11 @@
+﻿def sumMultipled3_5(begin,end):
+    sum=0
+    for i in range(begin, end):
+        if (i%3  == 0 or i%5 == 0):
+            sum=sum + i
+    print sum
+    return sum
+
+sumMultipled3_5(0,1001)
+
+raw_input()

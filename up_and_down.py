c63b9a22da3319d5cd750ed66c9897ea8966a767
ParkSoJeong <sj1801sj@naver.com>
2016-04-11 12:16:26 +0900

    Added files via upload

---
+++ up_and_down.py
@@ -0,0 +1,19 @@
+﻿def up_and_down():
+    target = input("pressed target: ")
+    count = 0
+    
+    while(1):
+        number = input("pressed number: ")
+        count = count+1
+        if(target == number):
+            print "biggo"
+            print count
+            break
+        else:
+            if(target>number):
+                print "up"
+            else:
+                print "down"
+                
+up_and_down()
+raw_input()

97194aeaee72be6276fa029bd147a89906f5ba6e
ParkSoJeong <sj1801sj@naver.com>
2016-04-11 21:03:56 +0900

    Added files via upload

---
+++ w6Main8.py
@@ -0,0 +1,37 @@
+﻿"""
+@author psj
+@since 160411
+"""
+
+
+def sumList(aList):
+    
+    for i in range(1,1001):
+        if(i%4==0 and i%5!=0):
+            print i
+            aList.append(i)
+
+    print len(aList)
+    sum=0
+
+    for i in range(0,len(aList)):
+        sum=sum+aList[i]
+
+    print sum
+
+
+
+def lab6():
+	aList=list()
+	sumList(aList)
+
+
+def main():
+	lab6()
+
+
+if __name__=="__main__": 
+	main() 
+ 
+ 
+raw_input() 

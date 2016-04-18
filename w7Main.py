d2df4ee7747809d8d67264ce09f453bf0b0e6230
ParkSoJeong <sj1801sj@naver.com>
2016-04-18 22:31:56 +0900

    Added files via upload

---
+++ w7Main.py
@@ -0,0 +1,36 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+
+def drawSquareAtSave(size, pos): 
+    tracks=list()
+    t1.penup() 
+    t1.setpos(pos) 
+    for i in range(0,4): 
+        t1.pendown()
+        t1.dot()
+        t1.penup()
+        t1.forward(size)
+        t1.pendown()
+        t1.dot()
+        t1.penup()
+        t1.right(90)
+        tracks.append(t1.pos())
+    return tracks
+
+def lab7():
+    size=100
+    pos=t1.pos()
+    mytracks=drawSquareAtSave(size,pos)
+    print mytracks
+
+
+
+def main():
+    lab7()
+
+if __name__=="__main__":
+    main()
+
+raw_input()

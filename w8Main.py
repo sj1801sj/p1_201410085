b5dcfa8d8e2ab5df4ee6eff8faab7ab80838e0a2
ParkSoJeong <sj1801sj@naver.com>
2016-05-02 13:39:03 +0900

    Added files via upload

---
+++ w8Main.py
@@ -0,0 +1,23 @@
+﻿def CharCount(word):
+    d=dict()
+    for c in word:
+        if c not in d:
+            d[c]=1
+        else:
+            d[c]=d[c]+1
+    print d
+    
+CharCount('parksojeong')
+
+
+
+def lab8():
+	word='parksojeong'
+
+def main():
+	lab8()
+
+if __name__=="__main__":
+	main()
+
+raw_input()

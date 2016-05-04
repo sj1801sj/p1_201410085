5ad48e1a6a1096d6c0448349fa459f84e59fd353
ParkSoJeong <sj1801sj@naver.com>
2016-05-04 11:21:13 +0900

    Update w8.2Main.py

--- w8.2Main.py
+++ w8.2Main.py
@@ -1,26 +1,26 @@
 ﻿def home():
 
-myhome={'TV', 'phone', 'camera', 'fridger', 'mixer', 'audio', 'cd player', 'light', 'computer', 'notebook', 'recorder'}
-yourhome={'coffee machine', 'radio', 'camera', 'running machine', 'ramp', 'computer', 'notebook', 'recorder'}
+  myhome={'TV', 'phone', 'camera', 'fridger', 'mixer', 'audio', 'cd player', 'light', 'computer', 'notebook', 'recorder'}
+  yourhome={'coffee machine', 'radio', 'camera', 'running machine', 'ramp', 'computer', 'notebook', 'recorder'}
 
-print yourhome - myhome
-print myhome - yourhome
-print myhome | yourhome
-print myhome & yourhome
+  print yourhome - myhome
+  print myhome - yourhome
+  print myhome | yourhome
+  print myhome & yourhome
 
-d=dict()
+  d=dict()
 
-for i in myhome:
-if i not in d:
-d[i]=1
-else:
-d[i]=d[i]+1
-for i in yourhome:
-if i not in d:
-d[i]=1
-else:
-d[i]=d[i]+1
-print d 
+  for i in myhome:
+    if i not in d:
+      d[i]=1
+    else:
+      d[i]=d[i]+1
+  for i in yourhome:
+    if i not in d:
+      d[i]=1
+    else:
+      d[i]=d[i]+1
+  print d 
 
 home()
 

9ab6a044d0c04dafe8c6c7ad35b00bfd60c040f6
ParkSoJeong <sj1801sj@naver.com>
2016-05-04 11:17:30 +0900

    Added files via upload

---
+++ w8.1Main.py
@@ -0,0 +1,38 @@
+﻿def adress():
+
+s='Hongjimoon streets of 20 February, Jongno-gu, Seoul'
+d1=dict()
+d2=dict()
+
+for word in s:
+asc = ord(word)
+if(48<=asc and asc<=57):
+if word not in d1:
+d1[word]=1
+else:
+d1[word]=d1[word]+1
+else:
+if word not in d2:
+d2[word]=1
+else:
+d2[word]=d2[word]+1
+
+print d1
+print d2
+
+d1.update(d2)
+
+%matplotlib inline
+import matplotlib
+import matplotlib.pyplot as plt
+
+plt.bar(range(len(d1)), d1.values(),align='center')
+plt.xticks(range(len(d1)), list(d1.keys()))
+plt.show() 
+
+
+adress()
+
+raw_input()
+
+
---
+++ w8.2Main.py
@@ -0,0 +1,27 @@
+﻿def home():
+
+myhome={'TV', 'phone', 'camera', 'fridger', 'mixer', 'audio', 'cd player', 'light', 'computer', 'notebook', 'recorder'}
+yourhome={'coffee machine', 'radio', 'camera', 'running machine', 'ramp', 'computer', 'notebook', 'recorder'}
+
+print yourhome - myhome
+print myhome - yourhome
+print myhome | yourhome
+print myhome & yourhome
+
+d=dict()
+
+for i in myhome:
+if i not in d:
+d[i]=1
+else:
+d[i]=d[i]+1
+for i in yourhome:
+if i not in d:
+d[i]=1
+else:
+d[i]=d[i]+1
+print d 
+
+home()
+
+raw_input()

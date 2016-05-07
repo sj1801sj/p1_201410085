23a6340924f30754e58e7230b5ab8f3a382809c7
ParkSoJeong <sj1801sj@naver.com>
2016-05-07 11:27:04 +0900

    Added files via upload

---
+++ w9.1Main.py
@@ -0,0 +1,23 @@
+﻿sumOfMen=0
+sumOfWomen=0
+avgOfMen=0
+avgOfWomen=0
+sumOfLocal=dict()
+for i in range(0,len(population)):
+    sumOfMen+=population[i][0]
+    sumOfWomen+=population[i][1]
+    avgOfMen=sumOfMen/25
+    avgOfWomen=sumOfWomen/25
+    sumOfLocal[i]=population[i][0]+population[i][1]
+
+print sumOfMen
+print sumOfWomen
+print avgOfMen
+print avgOfWomen
+print sumOfLocal
+
+%matplotlib inline
+import matplotlib
+import matplotlib.pyplot as plt
+
+plt.bar(range(len(population)),sumOfLocal.values())
---
+++ w9Main.py
@@ -0,0 +1,7 @@
+﻿import math
+for i in range(0,len(Local)):
+    q=math.sqrt((Local[i][0]-37.602637)**2+(Local[i][1]-126.955254)**2)
+    print q
+
+Local2=[0.0324182333418,0.039907346003,0.0375495177333,0.0283059783438,0.0177875672592]
+print min(Local2)
